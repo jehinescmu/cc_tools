532a93243822aa2012c942f54b813df7db4c77c5
Joseph Hines <jehines@andrew.cmu.edu>
2019-02-19 19:48:09 -0500

    Update to assignment
    Partially working only returning one game

--- part_2_read_test_json.py
+++ part_2_read_test_json.py
@@ -1,40 +1,37 @@
 import json
 import test_data
 
-#Creates and returns a GameLibrary object(defined in test_data) from loaded json_data
-def make_game_library_from_json( json_data ):
 
-
-
-
-
-    #Initialize a new GameLibrary
+# Creates and returns a GameLibrary object(defined in test_data) from loaded json_data
+def make_game_library_from_json(json_data):
+    # Initialize a new GameLibrary
     game_library = test_data.GameLibrary()
 
-    game_library.games = json_data["games"]
+    for game_json in json_data["games"]:
+        game = test_data.Game()
+        game.title = game_json["title"]
+        game.year = game_json["year"]
 
-    print(game_library.games)
+        for platform_json in game_json["platform"]:
+            game.platform = test_data.Platform()
+            game.platform.name = platform_json["name"]
+            game.platform.launch_year = platform_json["launch_year"]
 
-
-
-
-
-
+    game_library.add_game(game)
 
     return game_library
 
 
-#Part 2
+# Part 2
 input_json_file = "data/test_data.json"
 
 with open("data/test_data.json", "r") as reader:
     test_data_json = json.load(reader)
 
-print("JSON data:")
+print("JSON Data:")
 print(test_data_json)
-#
-# test_data = make_game_library_from_json(test_data_json)
-# print()
-# print("Test data:")
-# print(test_data)
 
+test_data = make_game_library_from_json(test_data_json)
+
+print("Test Data:")
+print(test_data)
